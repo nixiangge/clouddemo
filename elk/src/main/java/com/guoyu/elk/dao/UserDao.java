ae98cf55d2959f10a65dc339d5ecbadc010d84b1
“nixiangge” <gongxianxiang52@163.com>
2020-06-08 16:42:18 +0800

    项目初始化4

--- elk/src/main/java/com/guoyu/elk/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.guoyu.elk.dao;
-
-import com.guoyu.elk.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDao extends CrudRepository<User,String> {
-}
